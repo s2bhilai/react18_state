7c6d21a2cc16c7bfaaa186491d98b8d96d0b80b2
Subin <subinbs4a@gmail.com>
2023-11-20 08:47:14 +0300

    local state and remote state

---
+++ src/services/useFetch.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+const baseUrl = process.env.REACT_APP_API_BASE_URL;
+
+export default function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function init() {
+      try {
+        const response = await fetch(baseUrl + url);
+        if (response.ok) {
+          const json = await response.json();
+          setData(json);
+        } else {
+          throw response;
+        }
+        setData(response);
+      } catch (e) {
+        setError(e);
+      } finally {
+        setLoading(false);
+      }
+      // getProducts("shoes")
+      //   .then((response) => setProducts(response))
+      //   .catch((e) => setError(e))
+      //   .finally(() => setLoading(false));
+    }
+
+    init();
+  }, [url]);
+
+  return { data, error, loading };
+}
